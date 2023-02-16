3e3a59477d33e261fd2d33481f5cf213af50ef87
Patrick <doyle1175@gmail.com>
2023-02-15 22:02:46 -0500

    Define the prompts for the team member types

--- index.js
+++ index.js
@@ -58,3 +58,85 @@ class Intern extends Employee {
     return 'Intern';
   }
 }
+
+// Define the prompts for the team member types
+const managerPrompts = [
+    {
+      type: 'input',
+      name: 'name',
+      message: "Enter the team manager's name:",
+      validate: (value) => value ? true : "Please enter the team manager's name."
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: "Enter the team manager's employee ID:",
+      validate: (value) => value ? true : "Please enter the team manager's employee ID."
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "Enter the team manager's email address:",
+      validate: (value) => value ? true : "Please enter the team manager's email address."
+    },
+    {
+      type: 'input',
+      name: 'officeNumber',
+      message: "Enter the team manager's office number:",
+      validate: (value) => value ? true : "Please enter the team manager's office number."
+    }
+  ];
+
+  const engineerPrompts = [
+    {
+      type: 'input',
+      name: 'name',
+      message: "Enter the engineer's name:",
+      validate: (value) => value ? true : "Please enter the engineer's name."
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: "Enter the engineer's employee ID:",
+      validate: (value) => value ? true : "Please enter the engineer's employee ID."
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "Enter the engineer's email address:",
+      validate: (value) => value ? true : "Please enter the engineer's email address."
+    },
+    {
+      type: 'input',
+      name: 'github',
+      message: "Enter the engineer's GitHub username:",
+      validate: (value) => value ? true : "Please enter the engineer's GitHub username."
+    }
+  ];
+
+  const internPrompts = [
+    {
+      type: 'input',
+      name: 'name',
+      message: "Enter the intern's name:",
+      validate: (value) => value ? true : "Please enter the intern's name."
+    },
+    {
+      type: 'input',
+      name: 'id',
+      message: "Enter the intern's employee ID:",
+      validate: (value) => value ? true : "Please enter the intern's employee ID."
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: "Enter the intern's email address:",
+      validate: (value) => value ? true : "Please enter the intern's email address."
+      },
+      {
+      type: 'input',
+      name: 'school',
+      message: "Enter the intern's school name:",
+      validate: (value) => value ? true : "Please enter the intern's school name."
+      }
+      ];
